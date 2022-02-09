802b6480eeabe80a1b1b3e757e48d8e6b3d755f2
yq Gong <gong.yq17@gmail.com>
2022-02-09 22:33:38 +0000

    small change to update vue format

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,72 @@
 <template>
-    <div class="container">
-        <div>
-            <transition name="fade">
-                <router-view></router-view>
-            </transition>
-        </div>
+  <div class="container">
+    <div>
+      <transition name="fade">
+        <router-view></router-view>
+      </transition>
     </div>
+  </div>
 </template>
 
-<style>
-    .fade-enter-active, .fade-leave-active {
-      transition: opacity .5s
-    }
-    .fade-enter, .fade-leave-active {
-      opacity: 0
-    }
-</style>
-
 <script>
-
-    export default{
-    }
+export default {};
+// export default {
+//   name: "app",
+//   components: {},
+//   data: function () {
+//     return { myChart: {} };
+//   },
+//   methods: {
+//     chartConstructor: function () {
+//       const ctx = document.getElementById("myChart").getContext("2d");
+//       const myChart = new Chart(ctx, {
+//         type: "bar",
+//         data: {
+//           labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//           datasets: [
+//             {
+//               label: "# of Votes",
+//               data: [12, 19, 3, 5, 2, 3],
+//               backgroundColor: [
+//                 "rgba(255, 99, 132, 0.2)",
+//                 "rgba(54, 162, 235, 0.2)",
+//                 "rgba(255, 206, 86, 0.2)",
+//                 "rgba(75, 192, 192, 0.2)",
+//                 "rgba(153, 102, 255, 0.2)",
+//                 "rgba(255, 159, 64, 0.2)",
+//               ],
+//               borderColor: [
+//                 "rgba(255, 99, 132, 1)",
+//                 "rgba(54, 162, 235, 1)",
+//                 "rgba(255, 206, 86, 1)",
+//                 "rgba(75, 192, 192, 1)",
+//                 "rgba(153, 102, 255, 1)",
+//                 "rgba(255, 159, 64, 1)",
+//               ],
+//               borderWidth: 1,
+//             },
+//           ],
+//         },
+//         options: {
+//           scales: {
+//             y: {
+//               beginAtZero: true,
+//             },
+//           },
+//         },
+//       });
+//     },
+//   },
+// };
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
