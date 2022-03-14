8e3540ce00717b02590211988c30391da3695a76
LazyTestingName <38413475+yq-gong@users.noreply.github.com>
2022-03-14 15:40:07 +0000

    feat(app): recreating based on user stories given (#1)
    initial set up and preparation from original template

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,44 @@
 <template>
-    <div class="container">
-        <div>
-            <transition name="fade">
-                <router-view></router-view>
-            </transition>
-        </div>
+  <div class="container">
+    <div>
+      <transition name="fade">
+        <router-view></router-view>
+      </transition>
     </div>
+    <p>{{ counter + player }}</p>
+    <button @click="onClick">Cool Test Click Me!</button>
+  </div>
 </template>
 
-<style>
-    .fade-enter-active, .fade-leave-active {
-      transition: opacity .5s
-    }
-    .fade-enter, .fade-leave-active {
-      opacity: 0
-    }
-</style>
-
 <script>
-
-    export default{
-    }
+export default {
+  data() {
+    return { testInput: 20 };
+  },
+  computed: {
+    counter() {
+      return this.testInput + 50;
+    },
+    player() {
+      return this.testInput;
+    },
+  },
+  methods: {
+    onClick() {
+      this.testInput += 1;
+    },
+    
+  },
+};
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
