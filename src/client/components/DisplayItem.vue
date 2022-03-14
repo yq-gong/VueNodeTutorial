50952333baeb0369787d1ed3fce548c4641666d9
yq Gong <gong.yq17@gmail.com>
2022-03-14 17:04:56 +0000

    feat(form): remove unused and create a basic appearance

--- src/client/components/DisplayItem.vue
+++ src/client/components/DisplayItem.vue
@@ -1,59 +1,32 @@
 <template>
   <div>
-    <h1>Items</h1>
-
-    <div class="row">
-      <div class="col-md-10"></div>
-      <div class="col-md-2">
-        <router-link :to="{ name: 'CreateItem' }" class="btn btn-primary"
-          >Create Item</router-link
-        >
-      </div>
+    <div class="header">
+      <a
+        href="/"
+        data-testid="brand-logo"
+        class="btui-c-link btui-c-link--primary bgui-c-header__brand-logo-link"
+        rel=""
+        ><h4>RentalCars.com</h4>
+      </a>
     </div>
     <br />
-
-    <table class="table table-hover">
-      <thead>
-        <tr>
-          <td>ID</td>
-          <td>Item Name</td>
-          <td>Item Price</td>
-          <td>Actions</td>
-        </tr>
-      </thead>
-
-      <tbody>
-        <tr v-for="item in items" :key="item._id">
-          <td>{{ item._id }}</td>
-          <td>{{ item.name }}</td>
-          <td>{{ item.price }}</td>
-          <td>
-            <router-link
-              :to="{ name: 'EditItem', params: { id: item._id } }"
-              class="btn btn-primary"
-              >Edit</router-link
-            >
-          </td>
-          <td>
-            <button class="btn btn-danger" v-on:click="deleteItem(item._id)">
-              Delete
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <search-form />
   </div>
 </template>
 
 <script>
+import SearchForm from "./SearchForm.vue";
 export default {
+  components: {
+    SearchForm,
+  },
   data() {
     return {
       items: [],
     };
   },
 
-  created: function () {
+  created: function() {
     this.fetchItems();
   },
 
