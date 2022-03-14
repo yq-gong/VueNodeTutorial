22e876c08e1913c3a7338fc7cfd20fbcfbb28f41
yq Gong <gong.yq17@gmail.com>
2022-03-14 15:44:04 +0000

    fix(route):emergency fix

--- src/main.js
+++ src/main.js
@@ -1,33 +1,33 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
-import VueAxios from 'vue-axios';
-import axios from 'axios';
+import VueAxios from "vue-axios";
+import axios from "axios";
 Vue.use(VueAxios, axios);
 
-import App from './App.vue';
-import CreateItem from './components/CreateItem.vue';
-import DisplayItem from './components/DisplayItem.vue';
-import EditItem from './components/EditItem.vue';
+import App from "./App.vue";
+import CreateItem from "./client/components/CreateItem.vue";
+import DisplayItem from "./client/components//DisplayItem.vue";
+import EditItem from "./client/components/EditItem.vue";
 
 const routes = [
   {
-        name: 'CreateItem',
-        path: '/create/item',
-        component: CreateItem
-    },
-    {
-          name: 'DisplayItem',
-          path: '/',
-          component: DisplayItem
-      },
-      {
-            name: 'EditItem',
-            path: '/edit/:id',
-            component: EditItem
-        }
+    name: "CreateItem",
+    path: "/create/item",
+    component: CreateItem,
+  },
+  {
+    name: "DisplayItem",
+    path: "/",
+    component: DisplayItem,
+  },
+  {
+    name: "EditItem",
+    path: "/edit/:id",
+    component: EditItem,
+  },
 ];
 
-const router = new VueRouter({ mode: 'history', routes: routes });
-new Vue(Vue.util.extend({ router }, App)).$mount('#app');
+const router = new VueRouter({ mode: "history", routes: routes });
+new Vue(Vue.util.extend({ router }, App)).$mount("#app");
