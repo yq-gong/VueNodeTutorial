f5dc041c95a1ec7d77c1a05f50ca53381e71d51d
LazyTestingName <38413475+yq-gong@users.noreply.github.com>
2022-03-14 19:05:26 +0000

    feat(mock): add mock data and basic functions

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,8 @@
-import Axios from "axios";
+import axios from "axios";
 import { baseURL } from "./config/util";
 
-export async function getSearchResponse(numberOfResults,searchTerm) {
-  return Axios.get(
+export async function getSearchResponse(numberOfResults, searchTerm) {
+  return axios.get(
     `${baseURL}/FTSAutocomplete.do?solrIndex=fts_en&solrRows=${numberOfResults}&solrTerm=${searchTerm}`
   );
 }
